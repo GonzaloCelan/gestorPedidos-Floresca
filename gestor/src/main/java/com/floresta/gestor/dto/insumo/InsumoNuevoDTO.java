d9d0712a36d4eb5083bf4be7e3c6cc76f7925607
Gonzalo Celán <gon_celan@hotmail.com>
2025-11-05 20:50:31 -0300

    Cambios en el backend

--- gestor/src/main/java/com/floresta/gestor/dto/insumo/InsumoNuevoDTO.java
+++ gestor/src/main/java/com/floresta/gestor/dto/insumo/InsumoNuevoDTO.java
@@ -5,37 +5,29 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
-import lombok.Builder;
-import lombok.Data;
 
-@Data
-@Builder
-public class InsumoNuevoDTO {
 
-	
-	@NotNull
-    @FutureOrPresent
-    private LocalDate fecha;  
-	
-	@NotBlank
-    private String material;
-	
-	@NotNull
-    @Min(value = 1, message = "La cantidad mínima es 1")
-    private Double cantidad;
-	
-	
-    private String proveedor;
-    
-    @NotNull
-    @PositiveOrZero
-    private BigDecimal precioUnitario;
-    
-    @NotNull
-    @PositiveOrZero
-    private BigDecimal precioTotal;
-    
-}
+
+public record InsumoNuevoDTO(
+
+	    @NotNull
+	    @FutureOrPresent
+	    LocalDate fecha,
+
+	    @NotNull
+	    @Min(value = 1, message = "La cantidad mínima es 1")
+	    Double cantidad,
+
+	    String proveedor,
+
+	    @NotNull
+	    @PositiveOrZero
+	    BigDecimal precioUnitario,
+
+	    @NotNull
+	    @PositiveOrZero
+	    BigDecimal precioTotal
+	) {}
